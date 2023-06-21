2b85b35d57f48dc08b15db48e1ff77d4395a645d
Mike Choe <44465330+minkichoe@users.noreply.github.com>
2023-06-21 22:32:03 +0900

    chore: spring boot initializer (#1)

---
+++ src/main/kotlin/com/mikechoe/cocsi/CocsiApplication.kt
@@ -0,0 +1,11 @@
+package com.mikechoe.cocsi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CocsiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CocsiApplication>(*args)
+}
